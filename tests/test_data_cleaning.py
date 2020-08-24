aa6af03d706e94485cc1b88f20c168625c50bcff
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-24 12:26:05 +0100

    score to int vector func

--- tests/test_data_cleaning.py
+++ tests/test_data_cleaning.py
@@ -8,9 +8,9 @@ from src.data_cleaning import score_to_int
 def test_score_to_int():
     scores = pd.Series(['7-6(7) 6-0', '6-0 RET', '1-0 1-0', np.NaN])
 
-    # wraps 4 returned vectors as a matrix, each row is a returned vector
-    result = np.array(score_to_int(scores))
+    # wraps 4 returned vectors as a matrix, each column is a returned vector
+    result = np.array(score_to_int(scores)).T
 
     # only the first score should return valid values
-    np.testing.assert_array_equal(result[:, 0], [13, 2, 6, 0])
-    np.testing.assert_array_equal(result[:, 1:], np.NaN)
+    np.testing.assert_array_equal(result[0], [13, 2, 6, 0])
+    np.testing.assert_array_equal(result[1:], np.NaN)
