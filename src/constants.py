75efa96d8aa772eb8ff60b9b6714899f63ead3b8
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-24 16:25:25 +0100

    clean file path

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,7 @@ PIPELINE_DATA_FILE = 'pipeline_data'
 # raw data saved here:
 # 0 = filename, 1 = year from, 2 = year to
 RAW_DATA_FILE_PATH = 'data/01_raw/{0}_from_{1}_to_{2}.csv'
+CLEAN_DATA_FILE_PATH = 'data/02_clean/{0}_from_{1}_to_{2}.csv'
 
 # urls
 WTA_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_{}.csv"
