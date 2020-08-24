94d01d721544b3c2bb57e9ac41f28a3f51108112
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-24 13:26:49 +0100

    created pipeline file constant

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,5 @@
+PIPELINE_DATA_FILE = 'pipeline_data'
+
 # raw data saved here:
 # 0 = filename, 1 = year from, 2 = year to
 RAW_DATA_FILE_PATH = 'data/01_raw/{0}_from_{1}_to_{2}.csv'
@@ -6,5 +8,8 @@ RAW_DATA_FILE_PATH = 'data/01_raw/{0}_from_{1}_to_{2}.csv'
 WTA_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_{}.csv"
 ITF_URL = "https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_qual_itf_{}.csv"
 
-# various columns
+# various columns, jeff sackman columns prefixed with j
 SOURCE_COL = 'source'
+J_WINNER_COL = 'winner_name'
+J_LOSER_COL = 'loser_name'
+J_SCORE = 'score'
