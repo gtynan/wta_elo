3e093e856d24275f86fa9ad7bd5217a11f65cd6d
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-27 13:48:16 +0100

    running TODO tidy

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,41 +1,54 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime
+import logging
 
 from .constants import PIPELINE_DATA_FILE, YEAR_FROM, YEAR_TO, CLEAN_DATA_FILE_PATH, SURFACE_MAP, ROUND_ORDER, PARAMS, SOURCE_COL, J_SURFACE_COL, J_WINNER_COL, J_LOSER_COL, J_SCORE_COL, J_T_NAME, J_T_DATE, J_ROUND
-from .data_ingestion import get_raw_games
-from .data_cleaning import score_to_int, get_player_map, surface_to_one_hot, get_inferred_date
-from .model import elo
+from .data_ingestion.data_scraping import get_raw_games
+from .data_ingestion.data_cleaning import score_to_int, get_player_map, surface_to_one_hot, get_inferred_date
+from .model.model import elo
+from .model.model_output import get_rankings, get_model_calibration, get_model_performance
+from .logging_functions import timeit
 
 
-def run():
+@timeit
+def run(year_from: int, year_to: int, test_size: int):
+    """Runs pipeline
+    """
+
+    assert (year_to - year_from - test_size) > 0
 
     # scraping
-    raw_data = get_raw_games(PIPELINE_DATA_FILE, YEAR_FROM, YEAR_TO)
+    raw_data = get_raw_games(PIPELINE_DATA_FILE, year_from, year_to)
 
     # cleaning
     raw_data['inferred_date'] = get_inferred_date(raw_data, J_T_NAME, J_T_DATE, J_ROUND, ROUND_ORDER)
 
     player_map, raw_data[['WID', 'LID']] = get_player_map(raw_data, J_WINNER_COL, J_LOSER_COL)
 
-    raw_data = raw_data[raw_data[J_SURFACE_COL].notnull()]
     s_categories, s_data = surface_to_one_hot(raw_data[J_SURFACE_COL], SURFACE_MAP)
     raw_data[s_categories] = s_data
 
-    raw_data[['WGames', 'WSets', 'LGames', 'LSets']] = score_to_int(raw_data[J_SCORE_COL])
+    score_cols = ['WGames', 'WSets', 'LGames', 'LSets']
+    raw_data[score_cols] = score_to_int(raw_data[J_SCORE_COL])
 
-    clean_data = raw_data[['inferred_date', 'WID', 'LID', *s_categories, 'WGames', 'WSets', 'LGames', 'LSets',
-                           SOURCE_COL, 'tourney_name', 'winner_name', 'loser_name']].dropna(subset=['WGames', 'WSets', 'LGames', 'LSets'])
+    clean_data = raw_data[['inferred_date', 'WID', 'LID', *s_categories, *score_cols, SOURCE_COL,
+                           'tourney_name', 'winner_name', 'loser_name']].dropna(subset=score_cols)
 
     clean_data = clean_data.sort_values(by='inferred_date').reset_index(drop=True)
 
-    clean_data.to_csv(CLEAN_DATA_FILE_PATH.format(PIPELINE_DATA_FILE, YEAR_FROM, YEAR_TO))
+    clean_data.to_csv(CLEAN_DATA_FILE_PATH.format(PIPELINE_DATA_FILE, year_from, year_to))
 
-    date_cutoff = datetime.strptime('20180101', '%Y%m%d')
+    logging.info('DATA CLEANING COMPLETE')
 
-    train_data = clean_data[clean_data['inferred_date'] < date_cutoff].copy(
+    # modelling
+    logging.info('MODELING STARTED')
+
+    date_cutoff = datetime.strptime(f'{year_to - test_size}1231', '%Y%m%d')
+
+    train_data = clean_data[clean_data['inferred_date'] <= date_cutoff].copy(
         deep=True).sort_values(by='inferred_date').reset_index(drop=True)
-    test_data = clean_data[clean_data['inferred_date'] >= date_cutoff].copy(
+    test_data = clean_data[clean_data['inferred_date'] > date_cutoff].copy(
         deep=True).sort_values(by='inferred_date').reset_index(drop=True)
 
     player_abilities = np.full((len(player_map), len(s_categories) + 1), 1500.)
@@ -49,36 +62,24 @@ def run():
         itf_col=SOURCE_COL, player_abs=player_abilities, games_played=games_played, player_trend=player_trend,
         at_abilities=alltime_abilities)
 
-    np.testing.assert_array_equal(player_abilities, 1500.)
-
     test_abilities, test_games, test_trend, test_alltime, test_predictions = elo(
         params=PARAMS, data=test_data, date_col='inferred_date', w_id_col='WID', w_games_col='WGames',
         w_sets_col='WSets', l_id_col='LID', l_games_col='LGames', l_sets_col='LSets', surface_cols=s_categories,
         itf_col=SOURCE_COL, player_abs=train_abilities, games_played=train_games, player_trend=train_trend,
         at_abilities=train_alltime)
 
-    itf_predictions = test_predictions[test_data[SOURCE_COL] == 'I']
-    wta_predictions = test_predictions[test_data[SOURCE_COL] == 'W']
+    logging.info('MODELING COMPLETE')
 
-    print(f"""Total: {len(test_predictions)}
-        Overall accuracy: {len(test_predictions[test_predictions > 0.5]) / len(test_predictions)}
-        Overall brier score: {np.mean((1 - test_predictions)**2)}
-
-        WTA total: {len(wta_predictions)}
-        WTA accuracy: {len(wta_predictions[wta_predictions > 0.5]) / len(wta_predictions)}
-        WTA brier score: {np.mean((1 - wta_predictions)**2)}
-        WTA loglik: {np.log(wta_predictions).sum()}
-        
-        ITF total: {len(itf_predictions)}
-        ITF accuracy: {len(itf_predictions[itf_predictions > 0.5]) / len(itf_predictions)}
-        ITF brier score: {np.mean((1 - itf_predictions)**2)}
-
-        Train loglik: {(np.log(train_p)*(train_data[SOURCE_COL] == 'W')).sum()}""")
-
+    # output
     test_data['p'] = test_predictions
-    inv_map = {v: k for k, v in player_map.items()}
-
-    test_data['winner_name'] = test_data['WID'].map(inv_map)
-    test_data['loser_name'] = test_data['LID'].map(inv_map)
-
     test_data.to_csv('data/03_output/test_predictions.csv')
+
+    model_performance = get_model_performance(test_predictions, test_data[SOURCE_COL].values)
+    with open('data/03_output/test_model_performance.txt', 'w+') as txt:
+        txt.write(model_performance)
+
+    fitted_ranking = get_rankings(player_map, test_abilities, test_trend, test_alltime, s_categories)
+    fitted_ranking.to_csv('data/03_output/test_rankings.csv')
+
+    calibration_fig = get_model_calibration(test_predictions, test_data[SOURCE_COL].values)
+    calibration_fig.savefig('data/03_output/test_model_calibration.png')
