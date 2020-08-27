9c96458aefb086105f060dccc283ed5a0d5b9569
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-27 11:27:17 +0100

    restructured folder added timed logs

--- src/data_ingestion/data_scraping.py
+++ src/data_ingestion/data_scraping.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import logging
 
-from .constants import RAW_DATA_FILE_PATH, WTA_URL, ITF_URL, SOURCE_COL
+from ..constants import RAW_DATA_FILE_PATH, WTA_URL, ITF_URL, SOURCE_COL
+from ..logging_functions import timeit
 
 
+@timeit
 def get_raw_games(
         filename: str, year_from: int, year_to: int, save: bool = True) -> pd.DataFrame:
     """Gets all WTA and ITF games, given a filename will initially try and load if dataset exists otherwise will download and save
@@ -17,12 +19,11 @@ def get_raw_games(
     Returns:
         pd.DataFrame: raw dataframe of games with additional column `source` indicating origin
     """
-    logging.info('GET DATA')
     file_path = RAW_DATA_FILE_PATH.format(filename, year_from, year_to)
 
     try:
         data = pd.read_csv(file_path, index_col=0, encoding="ISO-8859-1", low_memory=False)
-        logging.info('GOT DATA: FROM FILE')
+        logging.debug('GOT DATA: FROM FILE')
 
     except:
         data = None
@@ -39,11 +40,11 @@ def get_raw_games(
                 data = new_wta
             data = data.append(new_itf, ignore_index=True)
 
-            logging.info(f'GOT DATA: {year}')
+            logging.debug(f'GOT DATA: {year}')
 
         if save:
             data.to_csv(file_path)
-            logging.info('DATA SAVED TO FILE')
+            logging.debug('DATA SAVED TO FILE')
 
-    logging.info(f'DATA LOADED: Total = {len(data)}')
+    logging.debug(f'DATA LOADED: Total = {len(data)}')
     return data
