b8a99092246ba60f0a161d4e1974b7e6ad7fb420
gavin_tynan <gavin.tynan1@gmail.com>
2020-08-27 13:10:18 +0100

    added visual model calibration

--- src/model/model_output.py
+++ src/model/model_output.py
@@ -1,6 +1,9 @@
 from typing import Dict, List
 import numpy as np
 import pandas as pd
+from sklearn.calibration import calibration_curve
+from copy import deepcopy
+import matplotlib.pyplot as plt
 
 from .model import get_current_ability, get_surface_weights
 from ..constants import PARAMS
@@ -30,3 +33,45 @@ def get_rankings(player_map: Dict[str, int],
             player_trend, PARAMS['trend_weight'])
 
     return rankings.sort_values(by=surfaces[0], ascending=False)
+
+
+def get_model_calibration(p: np.array, source: np.array) -> plt.figure:
+    """Generate visual of maodel calibration
+
+    Args:
+        p (np.array): Predicted probabilities for the winner
+        source (np.array): match source
+
+    Returns:
+        plt.figure: Model calibration visual
+    """
+    # 0.5 predcitions mess up bins
+    source = source[p != 0.5]
+    p = p[p != 0.5]
+
+    probs = deepcopy(p)
+    probs[probs < 0.5] = 1 - probs[probs < 0.5]
+
+    fration_of_positives, mean_prob = calibration_curve(y_true=p >= 0.5, y_prob=probs, n_bins=10)
+
+    f, ax = plt.subplots(figsize=(14, 7))
+
+    ax.plot(mean_prob, fration_of_positives, "s-", label='Overall')
+
+    fration_of_positives, mean_prob = calibration_curve(
+        y_true=p[source == 'W'] >= 0.5, y_prob=probs[source == 'W'], n_bins=10)
+
+    ax.plot(mean_prob, fration_of_positives, "s-", label='WTA')
+
+    fration_of_positives, mean_prob = calibration_curve(
+        y_true=p[source == 'I'] >= 0.5, y_prob=probs[source == 'I'], n_bins=10)
+
+    ax.plot(mean_prob, fration_of_positives, "s-", label='ITF')
+
+    ax.plot([0.5, 1], [0.5, 1], "k:", label="Perfectly calibrated")
+
+    ax.legend(loc="lower right")
+    ax.set_xlabel('Mean predicted value')
+    ax.set_ylabel('Fraction of positives')
+
+    return f
